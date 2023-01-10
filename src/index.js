6a30fe7dd7c300d4b5f6f19a166b106544fe883c
palmally <palmally@yandex.ru>
2023-01-10 20:12:59 +0300

    feat: fake router

--- src/index.js
+++ src/index.js
@@ -8,6 +8,67 @@ const chatsData = [
 ];
 
 const root = document.getElementById('root');
-root.innerHTML = tpl({ username: 'Алексей' });
 
-chatsData.forEach((chatData) => root.insertAdjacentHTML('beforeend', chat(chatData)));
+const routes = {};
+
+function chats() {
+  const ul = document.createElement('ul');
+  const link = document.createElement('a');
+  link.href = '#/';
+  link.textContent = 'на главную';
+  chatsData.forEach((chatData) => ul.insertAdjacentHTML('beforeend', chat(chatData)));
+  root.appendChild(ul);
+  root.appendChild(link);
+}
+
+function greeting() {
+  let div = document.createElement('div');
+  div.insertAdjacentHTML('afterbegin', tpl({ username: 'Ivan' }));
+  const link = document.createElement('a');
+  link.href = '#/chats';
+  link.textContent = 'чаты';
+  root.appendChild(div);
+  root.appendChild(link);
+}
+
+function notFound() {
+  const p = document.createElement('p');
+  p.textContent = '404 page not Found';
+  root.appendChild(p);
+  const link = document.createElement('a');
+  link.href = '#/';
+  link.textContent = 'на главную';
+  root.appendChild(link);
+}
+
+function route(path, template) {
+  if (typeof template === 'function') {
+    routes[path] = template;
+  }
+}
+
+route('/', greeting);
+route('/chats', chats);
+route('*', notFound);
+
+function resolveRoute(route) {
+  root.innerHTML = '';
+  if (!routes[route]) {
+    if (routes['*']) {
+      return routes['*'];
+    } else {
+      throw new Error(`Route ${route} doesn't exist!`);
+    }
+  }
+  return routes[route];
+}
+
+function router() {
+  let url = window.location.hash.slice(1) || '/';
+  let route = resolveRoute(url);
+
+  route();
+}
+
+window.addEventListener('DOMContentLoaded', router);
+window.addEventListener('hashchange', router);
